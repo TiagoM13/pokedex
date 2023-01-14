cc86fbe3315eb69c78af526eb70a585bf9062870
TiagoM13 <thyagomotha2000@gmail.com>
2023-01-14 15:46:40 -0300

    feat: api request to data

--- src/components/ListPokemon/index.tsx
+++ src/components/ListPokemon/index.tsx
@@ -1,35 +1,31 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { useState } from 'react';
+import React from 'react';
 
-import { POKEMONS } from '../../database/data';
-import { Pokemons } from '../../interfaces/pokemons';
-import { getPokemonSkills, getTypesPokemons } from '../../utils/pokemonsData';
-import { PokemonCard, PokemonData } from '../PokemonCard';
+import { useGetPokemonsData } from '../../hooks/usePokemons';
+import { PokemonCard } from '../PokemonCard';
 
-interface Props {
-  data: PokemonData[];
-}
+export const ListPokemon = () => {
+  const { pokemons } = useGetPokemonsData();
 
-export const ListPokemon = ({ data }: Props) => {
   return (
     <div>
       <ul className="grid grid-cols-6 gap-1 justify-items-center">
-        {data.map((pokemon) => {
+        {pokemons.map((pokemon) => {
           return (
             <PokemonCard
-              key={pokemon.order}
-              order={pokemon.order}
-              name={pokemon.name}
+              key={pokemon.data.id}
+              order={pokemon.data.order}
+              name={pokemon.data.name}
               skills={{
-                skill_1: pokemon.skills.skill_1,
-                skill_2: pokemon.skills.skill_2,
+                skill_1: pokemon.data.abilities[0]?.ability.name,
+                skill_2: pokemon.data.abilities[1]?.ability.name,
               }}
               types={{
-                type_1: pokemon.types.type_1,
-                type_2: pokemon.types.type_2,
+                type_1: pokemon.data.types[0].type.name,
+                type_2: pokemon.data.types[1]?.type.name,
               }}
-              url_img={pokemon.url_img}
+              url_img={pokemon.data.sprites.front_default}
             />
           );
         })}

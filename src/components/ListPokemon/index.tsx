c8dc01d5d33d9f92465fa7d7ee3479525237fbcd
TiagoM13 <thyagomotha2000@gmail.com>
2023-01-22 13:56:39 -0300

    feat: update database

--- src/components/ListPokemon/index.tsx
+++ src/components/ListPokemon/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { POKEMONS } from '../../database/data';
 import { ListProps } from '../../interfaces/list';
 import { getFirstLetterCapitalized } from '../../utils/getFirstLetterCapitalized';
 import { PokemonCard } from '../PokemonCard';
@@ -8,21 +9,21 @@ export const ListPokemon = ({ pokemons }: ListProps) => {
   return (
     <div>
       <ul className="grid grid-cols-6 gap-1 justify-items-center">
-        {pokemons.map((pokemon) => {
+        {POKEMONS.map((pokemon) => {
           return (
             <PokemonCard
-              key={pokemon.data.id}
-              order={pokemon.data.order}
-              name={getFirstLetterCapitalized(pokemon.data.name)}
+              key={pokemon.order}
+              order={pokemon.order}
+              name={getFirstLetterCapitalized(pokemon.name)}
               skills={{
-                skill_1: pokemon.data.abilities[0]?.ability.name,
-                skill_2: pokemon.data.abilities[1]?.ability.name,
+                skill_1: pokemon.skills?.skill_1,
+                skill_2: pokemon.skills?.skill_2,
               }}
               types={{
-                type_1: pokemon.data.types[0].type.name,
-                type_2: pokemon.data.types[1]?.type.name,
+                type_1: pokemon.types.type_1,
+                type_2: pokemon.types?.type_2,
               }}
-              url_img={pokemon.data.sprites.front_default}
+              url_img={pokemon.url_img}
             />
           );
         })}

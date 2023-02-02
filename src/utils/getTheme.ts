07ee2afb460e8d31c83c8ada308a2870b558ca9e
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-02 11:35:49 -0300

    refactor: refactoring features

--- src/utils/getTheme.ts
+++ src/utils/getTheme.ts
@@ -1,224 +1,27 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable consistent-return */
 import { THEME } from '../theme/theme';
 
 export const getTypesToColor = (type: string) => {
-  if (type === THEME.TYPES.GRASS) {
-    return THEME.COLORS.GRASS;
-  }
-
-  if (type === THEME.TYPES.FIRE) {
-    return THEME.COLORS.FIRE;
-  }
-
-  if (type === THEME.TYPES.WATER) {
-    return THEME.COLORS.WATER;
-  }
-
-  if (type === THEME.TYPES.BUG) {
-    return THEME.COLORS.BUG;
-  }
-
-  if (type === THEME.TYPES.FLYING) {
-    return THEME.COLORS.FLYING;
-  }
-
-  if (type === THEME.TYPES.DARK) {
-    return THEME.COLORS.DARK;
-  }
-
-  if (type === THEME.TYPES.DRAGON) {
-    return THEME.COLORS.DRAGON;
-  }
-
-  if (type === THEME.TYPES.ELECTRIC) {
-    return THEME.COLORS.ELECTRIC;
-  }
-
-  if (type === THEME.TYPES.FAIRY) {
-    return THEME.COLORS.FAIRY;
-  }
-
-  if (type === THEME.TYPES.FIGHTING) {
-    return THEME.COLORS.FIGHTING;
-  }
-
-  if (type === THEME.TYPES.GHOST) {
-    return THEME.COLORS.GHOST;
-  }
-
-  if (type === THEME.TYPES.GROUND) {
-    return THEME.COLORS.GROUND;
-  }
-
-  if (type === THEME.TYPES.ICE) {
-    return THEME.COLORS.ICE;
-  }
-
-  if (type === THEME.TYPES.NORMAL) {
-    return THEME.COLORS.NORMAL;
-  }
-
-  if (type === THEME.TYPES.POISON) {
-    return THEME.COLORS.POISON;
-  }
-
-  if (type === THEME.TYPES.PSYCHIC) {
-    return THEME.COLORS.PSYCHIC;
-  }
-
-  if (type === THEME.TYPES.ROCK) {
-    return THEME.COLORS.ROCK;
-  }
-
-  if (type === THEME.TYPES.STEEL) {
-    return THEME.COLORS.STEEL;
+  for (const [key, color] of Object.entries(THEME.COLORS)) {
+    if (type === key) {
+      return color;
+    }
   }
 };
 
 export const getTypesToBackgroundColor = (type: string) => {
-  if (type === THEME.TYPES.GRASS) {
-    return THEME.BACKGROUND.GRASS;
-  }
-
-  if (type === THEME.TYPES.FIRE) {
-    return THEME.BACKGROUND.FIRE;
-  }
-
-  if (type === THEME.TYPES.WATER) {
-    return THEME.BACKGROUND.WATER;
-  }
-
-  if (type === THEME.TYPES.BUG) {
-    return THEME.BACKGROUND.BUG;
-  }
-
-  if (type === THEME.TYPES.FLYING) {
-    return THEME.BACKGROUND.FLYING;
-  }
-
-  if (type === THEME.TYPES.DARK) {
-    return THEME.BACKGROUND.DARK;
-  }
-
-  if (type === THEME.TYPES.DRAGON) {
-    return THEME.BACKGROUND.DRAGON;
-  }
-
-  if (type === THEME.TYPES.ELECTRIC) {
-    return THEME.BACKGROUND.ELECTRIC;
-  }
-
-  if (type === THEME.TYPES.FAIRY) {
-    return THEME.BACKGROUND.FAIRY;
-  }
-
-  if (type === THEME.TYPES.FIGHTING) {
-    return THEME.BACKGROUND.FIGHTING;
-  }
-
-  if (type === THEME.TYPES.GHOST) {
-    return THEME.BACKGROUND.GHOST;
-  }
-
-  if (type === THEME.TYPES.GROUND) {
-    return THEME.BACKGROUND.GROUND;
-  }
-
-  if (type === THEME.TYPES.ICE) {
-    return THEME.BACKGROUND.ICE;
-  }
-
-  if (type === THEME.TYPES.NORMAL) {
-    return THEME.BACKGROUND.NORMAL;
-  }
-
-  if (type === THEME.TYPES.POISON) {
-    return THEME.BACKGROUND.POISON;
-  }
-
-  if (type === THEME.TYPES.PSYCHIC) {
-    return THEME.BACKGROUND.PSYCHIC;
-  }
-
-  if (type === THEME.TYPES.ROCK) {
-    return THEME.BACKGROUND.ROCK;
-  }
-
-  if (type === THEME.TYPES.STEEL) {
-    return THEME.BACKGROUND.STEEL;
+  for (const [key, background_color] of Object.entries(THEME.BACKGROUND)) {
+    if (type === key) {
+      return background_color;
+    }
   }
 };
 
 export const getTypesToIconsTypes = (type: string) => {
-  if (type === THEME.TYPES.GRASS) {
-    return THEME.ICONS.GRASS;
-  }
-
-  if (type === THEME.TYPES.FIRE) {
-    return THEME.ICONS.FIRE;
-  }
-
-  if (type === THEME.TYPES.WATER) {
-    return THEME.ICONS.WATER;
-  }
-
-  if (type === THEME.TYPES.BUG) {
-    return THEME.ICONS.BUG;
-  }
-
-  if (type === THEME.TYPES.FLYING) {
-    return THEME.ICONS.FLYING;
-  }
-
-  if (type === THEME.TYPES.DARK) {
-    return THEME.ICONS.DARK;
-  }
-
-  if (type === THEME.TYPES.DRAGON) {
-    return THEME.ICONS.DRAGON;
-  }
-
-  if (type === THEME.TYPES.ELECTRIC) {
-    return THEME.ICONS.ELECTRIC;
-  }
-
-  if (type === THEME.TYPES.FAIRY) {
-    return THEME.ICONS.FAIRY;
-  }
-
-  if (type === THEME.TYPES.FIGHTING) {
-    return THEME.ICONS.FIGHTING;
-  }
-
-  if (type === THEME.TYPES.GHOST) {
-    return THEME.ICONS.GHOST;
-  }
-
-  if (type === THEME.TYPES.GROUND) {
-    return THEME.ICONS.GROUND;
-  }
-
-  if (type === THEME.TYPES.ICE) {
-    return THEME.ICONS.ICE;
-  }
-
-  if (type === THEME.TYPES.NORMAL) {
-    return THEME.ICONS.NORMAL;
-  }
-
-  if (type === THEME.TYPES.POISON) {
-    return THEME.ICONS.POISON;
-  }
-
-  if (type === THEME.TYPES.PSYCHIC) {
-    return THEME.ICONS.PSYCHIC;
-  }
-
-  if (type === THEME.TYPES.ROCK) {
-    return THEME.ICONS.ROCK;
-  }
-
-  if (type === THEME.TYPES.STEEL) {
-    return THEME.ICONS.STEEL;
+  for (const [key, icon] of Object.entries(THEME.ICONS)) {
+    if (type === key) {
+      return icon;
+    }
   }
 };

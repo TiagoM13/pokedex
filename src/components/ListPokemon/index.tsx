a11d01fd1416e4fe3de5199942ca495391b3e8ea
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-19 12:22:47 -0300

    refactor: separate button into a  and props

--- src/components/ListPokemon/index.tsx
+++ src/components/ListPokemon/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { MdOutlineCatchingPokemon } from 'react-icons/md';
 
 import { usePagination } from '../../hooks/usePagination';
 import { useGetPokemonsData } from '../../hooks/usePokemons';
@@ -8,6 +7,7 @@ import { Pokemons } from '../../interfaces/pokemons';
 import { getFirstLetterCapitalized } from '../../utils/getFirstLetterCapitalized';
 import { Loading } from '../Loading/Loading';
 import { PokemonCard } from '../PokemonCard';
+import { ShowMoreButton } from '../ShowMoreButton/ShowMoreButton';
 
 export const ListPokemon = ({ data }: ListProps) => {
   const itemsPerPage = 20;
@@ -47,18 +47,7 @@ export const ListPokemon = ({ data }: ListProps) => {
       )}
 
       {currentItems.length < data.length && (
-        <button
-          className="flex gap-2 items-center mx-auto bg-[#fe6613] py-2 px-4 my-4 text-white text-base border-none  rounded-lg hover:brightness-95 hover:scale-105 transition-all disabled:cursor-not-allowed disabled:scale-100 disabled:brightness-100"
-          type="button"
-          onClick={showMoreItems}
-          disabled={loadItems}
-        >
-          <MdOutlineCatchingPokemon
-            size={25}
-            className={`text-white ${loadItems && 'animate-loading'}`}
-          />
-          {loadItems ? 'Loading pokemons...' : 'Show more pokemons'}
-        </button>
+        <ShowMoreButton showMoreItems={showMoreItems} loadItems={loadItems} />
       )}
     </div>
   );

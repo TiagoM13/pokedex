3390e450c35058414ede0d2292a48fde628a43b2
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-19 11:52:19 -0300

    fix: set app element

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Modal from 'react-modal';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './routes/route_app';
+
+Modal.setAppElement('#root');
 
 const App = () => {
   return <RouterProvider router={router} />;

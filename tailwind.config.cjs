57ad7ea53c7785e63a5dc254a0b90cb082694cd4
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-19 10:50:55 -0300

    feat: add animate modal

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,10 @@ module.exports = {
         orange: "url('./src/assets/background/background_orange.png')",
       },
       keyframes: {
+        modal: {
+          '0%': { opacity: 0, transform: 'translate(-50%, 0%)' },
+          '100%': { opacity: 1, transform: 'translate(-50%, -50%)' },
+        },
         reveal: {
           '0%': { opacity: 0, transform: 'translate(0, 40%)' },
           '100%': { opacity: 1, transform: 'translate(0, 0)' },
@@ -23,6 +27,7 @@ module.exports = {
       animation: {
         reveal: 'reveal 0.3s ease-out',
         loading: 'loading 0.6s linear infinite',
+        modal: 'modal 0.6s ease-out',
       },
     },
   },

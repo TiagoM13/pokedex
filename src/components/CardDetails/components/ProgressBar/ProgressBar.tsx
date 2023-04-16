1bd097df93fc8b38833d651817ee45e6fb6ffee5
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-16 15:25:42 -0300

    feat: add new component - ProgressBar

---
+++ src/components/CardDetails/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+type IProgressBar = {
+  amount: number;
+};
+
+export const ProgressBar = ({ amount }: IProgressBar) => {
+  const [progress, setProgress] = React.useState(0);
+  const maxPercentage = 100;
+
+  let percentage = (amount / 100) * maxPercentage;
+
+  if (percentage > 100) {
+    percentage = 100;
+  }
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setProgress(percentage), 500);
+    return () => clearTimeout(timer);
+  }, [percentage]);
+
+  return (
+    <div role="progressbar" className="h-2 w-full bg-zinc-400 rounded">
+      <div
+        className="h-2 rounded-xl bg-green-400 transition-all duration-500"
+        style={{
+          width: `${progress}%`,
+        }}
+      />
+    </div>
+  );
+};

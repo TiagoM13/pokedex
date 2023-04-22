35c65fa97b4729740c948b9413439913edf58499
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-22 12:39:17 -0300

    refactor: removing code and simplifying logic

--- src/components/DetailsCard/components/Tabs/Tabs.tsx
+++ src/components/DetailsCard/components/Tabs/Tabs.tsx
@@ -1,42 +1,29 @@
 import React from 'react';
 
-type TabsProps = {
-  select: boolean;
-  selectTwo: boolean;
-  onSelect: () => void;
+type TabsProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  title: string;
+  isActive?: boolean;
+  status: 'open' | 'closed';
 };
 
-export const Tabs = ({ select, selectTwo, onSelect }: TabsProps) => {
-  const buttonSelect = {
-    borderBottom: select ? '2px solid #000000' : '',
-    color: select ? '#000000' : '',
-    fontWeight: select ? 'bold' : '',
-  };
-
-  const buttonSelectTwo = {
-    borderBottom: selectTwo ? '2px solid #000000' : '',
-    color: selectTwo ? '#000000' : '',
-    fontWeight: selectTwo ? 'bold' : '',
-  };
-
+export const Tabs = ({
+  title,
+  isActive = false,
+  status,
+  ...rest
+}: TabsProps) => {
   return (
-    <div className="flex border-b border-zinc-300 mx-4">
-      <button
-        type="button"
-        onClick={onSelect}
-        className="h-11 flex flex-1 px-5 items-center justify-center text-sm"
-        style={buttonSelect}
-      >
-        Info
-      </button>
-      <button
-        type="button"
-        onClick={onSelect}
-        className="h-11 flex flex-1 px-5 items-center justify-center text-sm"
-        style={buttonSelectTwo}
-      >
-        Stats
-      </button>
-    </div>
+    <button
+      {...rest}
+      type="button"
+      className="h-11 flex flex-1 px-5 items-center justify-center text-sm"
+      style={{
+        borderBottom: isActive ? '2px solid #000000' : '',
+        color: isActive ? '#000000' : '',
+        fontWeight: isActive ? 'bold' : '',
+      }}
+    >
+      {title}
+    </button>
   );
 };

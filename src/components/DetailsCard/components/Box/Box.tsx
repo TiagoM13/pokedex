b63072b2b8ffc1a1a21611d1225af1fad9664082
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-23 18:10:59 -0300

    feat: add new component

---
+++ src/components/DetailsCard/components/Box/Box.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type BoxProps = {
+  text: string | number;
+};
+
+export const Box = ({ text }: BoxProps) => {
+  return (
+    <div className="text-center border border-zinc-300 rounded-lg py-2 px-3 mb-2">
+      <span className="text-base font-medium capitalize">{text}</span>
+    </div>
+  );
+};

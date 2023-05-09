638626815d8fdc66444dac2c105601a5bca29173
TiagoM13 <thyagomotha2000@gmail.com>
2023-05-09 11:33:17 -0300

    feat: add media queries to component

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -8,13 +8,25 @@ export const SocialMedia: React.FC = () => {
   return (
     <div className="flex gap-1 items-center">
       <Anchor url="https://github.com/TiagoM13">
-        <GithubLogo size={25} weight="bold" />
+        <GithubLogo
+          size={25}
+          weight="bold"
+          className="screen-1x:w-5 screen-1x:h-5"
+        />
       </Anchor>
       <Anchor url="https://www.linkedin.com/in/tiago-mota-4690591a8/">
-        <LinkedinLogo size={25} weight="bold" />
+        <LinkedinLogo
+          size={25}
+          weight="bold"
+          className="screen-1x:w-5 screen-1x:h-5"
+        />
       </Anchor>
       <Anchor url="https://twitter.com/tiago_m13">
-        <TwitterLogo size={25} weight="bold" />
+        <TwitterLogo
+          size={25}
+          weight="bold"
+          className="screen-1x:w-5 screen-1x:h-5"
+        />
       </Anchor>
     </div>
   );

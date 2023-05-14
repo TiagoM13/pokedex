09cfd7ccc268d3aec403295211dbcf994e58c82e
TiagoM13 <thyagomotha2000@gmail.com>
2023-05-14 14:45:15 -0300

    feat: setting environment variable in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import EnvironmentPlugin from 'vite-plugin-environment';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), EnvironmentPlugin('all')],
 });

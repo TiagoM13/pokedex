e316c6ae888de2adfe251f9105bb6698b97846c6
TiagoM13 <thyagomotha2000@gmail.com>
2023-05-28 18:32:21 -0300

    feat: add fake request to api in test

--- _tests_/pages/pokedex.spec.tsx
+++ _tests_/pages/pokedex.spec.tsx
@@ -3,9 +3,20 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Pokedex } from '@pages';
 import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
 describe('<Pokedex />', () => {
-  it('should render the page Pokedex', () => {
+  it('should render the page Pokedex', async () => {
+    const mockPokemons = [
+      { id: 1, name: 'Pikachu' },
+      { id: 2, name: 'Charmander' },
+      // Adicione mais objetos de pokemons mockados, se necessário
+    ];
+
+    const mock = new MockAdapter(axios);
+    mock.onGet().reply(200, mockPokemons);
+
     render(
       <>
         <BrowserRouter>
@@ -13,6 +24,9 @@ describe('<Pokedex />', () => {
         </BrowserRouter>
       </>
     );
+
+    await screen.findByText('Select your pokemon');
+
     expect(screen.getByText('Select your pokemon')).toBeInTheDocument();
   });
 });

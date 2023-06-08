d7bea41e1dd7822d7116772e74564d52abddef14
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-08 14:28:17 -0300

    refactor: use context to access filter function, format style classe

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 
-import { ISearchButton } from '@interfaces';
+import { usePokemonContext } from '@contexts';
 import { MagnifyingGlass } from 'phosphor-react';
 
-export const InputSearch = ({ getFilterPokemon }: ISearchButton) => {
+export const InputSearch = () => {
+  const { filterPokemon } = usePokemonContext();
+
   return (
-    <div className="flex items-center flex-col gap-2 py-6 px-1 mx-4 mb-2">
-      <div className="flex flex-col items-center gap-1 relative">
+    <div className="mx-4 mb-2 flex flex-col items-center gap-2 py-6 px-1">
+      <div className="relative flex flex-col items-center gap-1">
         <label htmlFor="search" className="text-2xl font-semibold">
           Select your pokemon
         </label>
-        <div className="flex justify-center w-[400px] screen-1x:w-screen screen-1x:px-8">
+        <div className="flex w-[400px] justify-center screen-1x:w-screen screen-1x:px-8">
           <input
-            onChange={(e) => getFilterPokemon(e.target.value)}
+            onChange={(e) => filterPokemon(e.target.value)}
             id="search"
             type="text"
             placeholder="Search pokemons..."
-            className="w-full text-zinc-500 border border-zinc-400 bg-zinc-100 rounded-xl mx-1 py-2 px-2 shadow-sm focus:outline-none focus:border-zinc-600 placeholder:italic placeholder:text-zinc-500"
+            className="mx-1 w-full rounded-xl border border-zinc-400 bg-zinc-100 py-2 px-2 text-zinc-500 shadow-sm placeholder:italic placeholder:text-zinc-500 focus:border-zinc-600 focus:outline-none"
           />
-          <span className="inline-block px-2 py-3 -ml-10">
+          <span className="-ml-10 inline-block px-2 py-3">
             <MagnifyingGlass
               size={20}
               weight="bold"

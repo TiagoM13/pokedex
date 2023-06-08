0d2a622a7876e749fff3c1e52456cadc27a99cbb
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-08 14:36:51 -0300

    refactor: use context in pokemon card component

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import { TypeCard, ImageLazyLoader } from '@components';
+import { usePokemonContext } from '@contexts';
 import { IPokemonCard } from '@interfaces';
 import {
   getNumberOrderFormat,
@@ -10,21 +11,19 @@ import {
 } from '@utils';
 
 export const PokemonCard = ({
-  name,
-  id,
-  types,
-  url_img,
-  getSelectedPokemon,
+  pokemon
 }: IPokemonCard) => {
+  const { selectPokemon } = usePokemonContext();
+
   return (
     <li className="w-full">
       <button
         type="button"
-        title={`Pokémon - ${name.toUpperCase()}`}
-        onClick={() => getSelectedPokemon(id)}
+        title={`Pokémon - ${pokemon.data.name.toUpperCase()}`}
+        onClick={() => selectPokemon(pokemon.data.id)}
         style={{
-          backgroundColor: types[0].type.name
-            ? getTypesToBackgroundColor(types[0].type.name.toUpperCase())
+          backgroundColor: pokemon.data.types[0].type.name
+            ? getTypesToBackgroundColor(pokemon.data.types[0].type.name.toUpperCase())
             : '#9d9b9b',
         }}
         className="w-full h-[160px] flex justify-between rounded-3xl p-2 my-1 text-white duration-500 hover:shadow-md hover:shadow-slate-400 hover:brightness-100 hover:-translate-y-1 animate-reveal screen-3x:h-[200px]"
@@ -32,14 +31,14 @@ export const PokemonCard = ({
         <div className="py-4 px-1">
           <div className="flex flex-col items-start">
             <h3 className="font-black line-clamp-1 text-sm capitalize screen-2x:text-xl">
-              {name}
+              {pokemon.data.name}
             </h3>
             <span className="font-semibold text-xs text-left screen-2x:text-lg">
-              {getNumberOrderFormat(id)}
+              {getNumberOrderFormat(pokemon.data.id)}
             </span>
           </div>
           <div className="pt-2 font-semibold text-[10px] screen-2x:text-sm">
-            {types.map((type) => {
+            {pokemon.data.types.map((type) => {
               return (
                 <TypeCard
                   key={type.type.name}
@@ -53,7 +52,7 @@ export const PokemonCard = ({
           </div>
         </div>
         <div className="flex items-center h-full w-full screen-2x:w-auto">
-          <ImageLazyLoader imageUrl={url_img} name={name} />
+          <ImageLazyLoader imageUrl={pokemon.data.sprites.other['official-artwork'].front_default} name={pokemon.data.name} />
         </div>
       </button>
     </li>

9798b3e4b42541db6d1ebb804eaefe646cf63cbe
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-09 12:06:15 -0300

    feat: create new custom hooks from contexts

---
+++ src/hooks/usePokemonContext.ts
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+
+import { PokemonContext } from '@contexts';
+
+export const usePokemonContext = () => {
+  const context = useContext(PokemonContext);
+  if (!context) {
+    throw new Error('usePokemonContext must be used within a PokemonProvider');
+  }
+  return context;
+};

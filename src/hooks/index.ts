64edf79397db19e630ca02a4554576d78df4298d
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-09 12:06:43 -0300

    feat: update hook exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,5 @@ export * from './usePokemons';
 export * from './useSelectPokemon';
 export * from './usePagination';
 export * from './useImageLazyLoader';
+export * from './useSelectedPokemonContext';
+export * from './usePokemonContext';

766ded9e081619c419185cb4b16b0bb5c6bdb49d
TiagoM13 <thyagomotha2000@gmail.com>
2023-08-30 21:43:49 -0300

    feat(tests): add tests to component Separator

---
+++ src/components/Separator/_tests_/Separator.spec.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+
+import { PokemonProvider } from '@contexts';
+import { act, render } from '@testing-library/react';
+
+import { Separator } from '../Separator';
+
+describe('Separator', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it('displays the correct Pokémon count', () => {
+    const mockContext = {
+      loading: true,
+      pokemons: [{}, {}, {}],
+    };
+
+    const usePokemonContext = jest.fn();
+
+    act(() => {
+      usePokemonContext.mockReturnValue(mockContext);
+    });
+
+    const { getByTestId } = render(
+      <PokemonProvider>
+        <Separator />
+      </PokemonProvider>
+    );
+
+    const countText = getByTestId('pokemon-count');
+    expect(countText).toHaveTextContent(`0 Pokémon`);
+  });
+});

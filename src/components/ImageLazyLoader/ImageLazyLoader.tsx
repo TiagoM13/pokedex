59eb1f76e78745a7a006d737d26a7a6454287ddf
TiagoM13 <thyagomotha2000@gmail.com>
2023-08-30 21:25:59 -0300

    feat(tests): add data-testid and fix tailwind css class

--- src/components/ImageLazyLoader/ImageLazyLoader.tsx
+++ src/components/ImageLazyLoader/ImageLazyLoader.tsx
@@ -12,13 +12,16 @@ export const ImageLazyLoader = ({ imageUrl, name }: ImageProps) => {
 
   if (isLoading) {
     return (
-      <div className="ml-auto h-[90px] w-[90px] animate-pulse rounded-full bg-gray-300/50 screen-3x:h-[140px] screen-3x:w-[140px] screen-2x:h-36 screen-2x:w-36" />
+      <div
+        data-testid="loading-animation"
+        className="ml-auto h-[90px] w-[90px] animate-pulse rounded-full bg-gray-300/50 screen-3x:h-[140px] screen-3x:w-[140px] screen-2x:h-36 screen-2x:w-36"
+      />
     );
   }
 
   return (
     <img
-      className="object-cotain w-full overflow-hidden screen-2x:max-w-[200px]"
+      className="w-full overflow-hidden object-contain screen-2x:max-w-[200px]"
       src={imageSrc || ''}
       alt={`Pokémon - ${name}`}
       loading="lazy"
